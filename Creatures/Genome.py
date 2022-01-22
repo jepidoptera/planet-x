cc76b2da1884820fd850c379aa0acab3805d3f81
jepidoptera <jarymoth@gmail.com>
2022-01-22 15:34:44 -0600

    building in trade-offs in stats

--- Creatures/Genome.py
+++ Creatures/Genome.py
@@ -3,6 +3,7 @@ def __init__(self, size, strength, speed, fortitude, intelligence, longevity, fe
 
     self.__size = size
     self.__strength = strength
+    self.__speed = speed
     self.__fortitude = fortitude
     self.__intelligence = intelligence
     self.__longevity = longevity
@@ -19,21 +20,36 @@ def __init__(self, size, strength, speed, fortitude, intelligence, longevity, fe
 
     return
 
-    @property 
-    def metabolism(self):
-        return self.__metabolism + (intelligence + strength + speed + size) / 4
+@property 
+def metabolism(self):
+    return self.__metabolism + (self.__intelligence + self.__strength + self.__speed + self.__size) / 4
 
-    @property
-    def size(self):
-        return self.__strength - self.__fertility / 4
+@property
+def size(self):
+    return self.__strength - self.__fertility / 4
 
-    @property
-    def fertility(self):
-        return self.__longevity / 3
+@property
+def fertility(self):
+    return self.__longevity / 3
 
-    @property
-    def metabolism(self):
-        return (self.__intelligence + self.__strength + self.__speed + self.__size) / 4
+@property
+def metabolism(self):
+    return (self.__intelligence + self.__strength + self.__speed + self.__size) / 4
+
+@property
+def strength(self):
+    strength = self.__strength - self.__speed / 3 - fertility / 4
+    strength += size / 2
+    return strength
+
+@property
+def speed(self):
+    return (self.__speed - self.__strength / 2) * 1.5
+
+@property
+def longevity(self):
+    longevity = self.__longevity / min(self.__size / self.__strength, 1)
+    return longevity
     # // longevity declines when strength < size
     # weakness = size / strength
     # longevity /= weakness
