232b8f213e35c29537c1530d2860844ac4870831
jepidoptera <jarymoth@gmail.com>
2022-02-03 22:58:55 -0600

    some thoughts

--- Creatures/Creature.py
+++ Creatures/Creature.py
@@ -1,12 +1,43 @@
 # good start
 # you made a file
+from ast import Return
+import Genome
+import Instinct
+class Neuron():
+    def __init__(self):
+        self.action = Return
 
-def __init__(self, size = 1.0, health = 1.0, fatstore = 1.0, instinct = (), genome = ()):
+# inputs:
+# for each visible world node
+# see creature (details about creature)
+# - speed
+# - deadliness
+# - fatness
+# - similarity
+# - mode
+# see food: meat, fruit, grass
+# self.mode == 1
+# self.mode == 2
+# self.mode == ...
+# self.sprint_moves
+# self.energy
+# 
+
+# apply some function:
+# * some factor
+
+# middle layer of maybe 12 hidden neurons
+
+# outputs:
+# fight fuck flee feast foray
+# herd
+# scan
+
+def __init__(self, fatstore: float, instinct: Instinct, genome: Genome):
     self.age = 0
-    self.size = size
     self.fatstore = fatstore
-    self.health = health
     self.brain = instinct
+    self.genome = genome
     # *age
     # *wisdom
     # *hunger
