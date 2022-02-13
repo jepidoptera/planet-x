d972992350613b2f858e3d30be27c17bd1b50b9d
jepidoptera <jarymoth@gmail.com>
2022-02-13 12:35:21 -0600

    working on brain structure

--- Creatures/Creature.py
+++ Creatures/Creature.py
@@ -1,50 +1,127 @@
 # good start
 # you made a file
-from ast import Return
-import Genome
-import Instinct
+from enum import Enum
+from random import random
+from genome import Genome
+from world.map import *
+
+class NeuronType(Enum):
+    creature = 0
+    self = 1
+    memory = 2
+    environment = 3
+    hidden = 4
+    action = 5
+    
 class Neuron():
-    def __init__(self):
-        self.action = Return
+    def __init__(self, type: NeuronType ):
+        self.activation = 0.0
+        self.sense = None
+        self.action = None
+        self.type = type
 
-# inputs:
-# for each visible world node
-# see creature (details about creature)
-# - speed
-# - deadliness
-# - fatness
-# - similarity
-# - mode
-# see food: meat, fruit, grass
-# self.mode == 1
-# self.mode == 2
-# self.mode == ...
-# self.sprint_moves
-# self.energy
-# 
+class Axon():
+    def __init__(self, input: Neuron, output: Neuron, factor: float):
+        self.input = input
+        self.output = output
+        self.factor = factor
 
-# apply some function:
-# * some factor
+class Brain():
+    def __init__(self, axons: list[Axon], neurons: list[Neuron]):
+        self.axons = axons
+        self.neurons = neurons
 
-# middle layer of maybe 12 hidden neurons
+class Creature():    
+    def __init__(self, world:Map, location:MapNode, genome:Genome):
+        self.world = world
+        
+        self.genome = genome
+        self.deadliness = genome.deadliness
+        self.speed = genome.speed
+        self.fortitude = genome.fortitude
+        self.stamina = genome.stamina
+        self.meatEating = genome.meatEating
+        self.plantEating = genome.plantEating
+        self.intelligence = genome.intelligence
+        self.sightRange = genome.sightRange
+        self.peripheralVision = genome.peripheralVision
 
-# outputs:
-# fight fuck flee feast foray
-# herd
-# scan
+        self.age = 0
+        self.health = self.fortitude
 
-def __init__(self, fatstore: float, instinct: Instinct, genome: Genome):
-    self.age = 0
-    self.fatstore = fatstore
-    self.brain = instinct
-    self.genome = genome
-    # *age
-    # *wisdom
-    # *hunger
-    # *health
-    # *stamina
+        self.location = location
+        self.direction = int(random.random() * len(self.location.neighbors))
+        # *age
+        # *wisdom
+        # *hunger
+        # *health
+        # *stamina
 
-    return
-def genotype():
-    # add up all its attributes into a long string
-    return
+        self.creatureNeurons = [
+            Neuron(NeuronType.creature) * 10
+        ]
+        self.selfNeurons = [
+            Neuron(NeuronType.self) * 5
+        ]
+        self.envNeurons = [
+            Neuron(NeuronType.environment) * 6
+        ]
+        self.memNeurons = [
+            Neuron(NeuronType.memory) * 8
+        ]
+        self.hiddenNeurons = [
+            Neuron(NeuronType.hidden) * 12
+        ]
+        self.actionNeurons = [
+            Neuron(NeuronType.action)
+        ]
+        self.brain = Brain(
+            neurons=[
+                *self.creatureNeurons,
+                *self.selfNeurons,
+                *self.envNeurons,
+                *self.memNeurons,
+                *self.hiddenNeurons,
+                *self.actionNeurons
+            ],
+            axons=[
+
+            ]
+        )
+        return 
+
+    def processEnvironment(self):
+        vision = self.getVisionRanges()
+
+        for layer, distance in enumerate(vision):
+            for node in layer:
+                self.processMapNode(node)
+
+    def processMapNode(self, node: MapNode):
+        for neuron in self.brain.neurons:
+            neuron.activation = 0.0
+
+        if type(node.occupant) == Creature:
+            other = node.occupant
+
+            for axon in self.brain.axons:
+                if axon.input.type == NeuronType.creature:
+                    axon.output.activation += axon.input.activation * axon.factor
+
+
+    def getVisionRanges(self):
+        cones = [
+            self.nodes[self.location].visionTree[(self.direction + n) % len(self.location.neighbors)][:self.sightRange] 
+            for n in [-int(n / 2) if n % 2 == 0 else int(n / 2) + 1 
+            for n in range((self.peripheralVision - 1) * 2 + 1)] 
+        ]
+        visionLayers = [
+            # get a separate array of nodes for each distance from self.location
+            [node.index for cone in [[cones[a][b] 
+            for a in range(len(cones))] for b in range(self.sightRange)][i] 
+            for node in cone] for i in range(self.sightRange)
+        ]
+        return visionLayers
+
+    def unpackBrainGenome(self):
+        return []
