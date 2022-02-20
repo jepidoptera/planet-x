c5dea3c0c83008e1ed1324a275432ebcbe13c110
jepidoptera <jarymoth@gmail.com>
2022-02-20 01:25:20 -0600

    debug

--- Creatures/templates.py
+++ Creatures/templates.py
@@ -2,20 +2,20 @@ from creatures.creature import *
 from creatures.genome import *
 
 basicBrain = [
-    Axon(netIndex['creature_deadliness'], netIndex['flee'], 1.0),
-    Axon(netIndex['similarity'], netIndex['action_mate'], 1.0),
+    Axon(netIndex['creature_deadliness'], netIndex['action_flee'], 1.0),
+    Axon(netIndex['creature_similarity'], netIndex['action_mate'], 1.0),
     Axon(netIndex['grass'], netIndex['action_eat'], 1.0),
-    Axon(netIndex['birth'], netIndex['memory_7'], 1.0),
+    Axon(netIndex['self_birth'], netIndex['memory_7'], 1.0),
     Axon(netIndex['memory_7'], netIndex['relay_7'], 1.0),
     Axon(netIndex['self_energy'], netIndex['action_eat'], -0.2),
     Axon(netIndex['relay_7'], netIndex['action_mate'], -1.0),
     Axon(netIndex['self_energy'], netIndex['relay_7'], -1.0),
-    Axon(netIndex['self_sprints'], netIndex['rest'], 0.5),
+    Axon(netIndex['self_sprints'], netIndex['action_rest'], 0.5),
     Axon(netIndex['self_injury'], netIndex['memory_5'], 1.0),
-    Axon(netIndex['memory_5'], netIndex['rest'], -0.4),
+    Axon(netIndex['memory_5'], netIndex['action_rest'], -0.4),
     Axon(netIndex['action_rest'], netIndex['memory_5'], 1.0),
 ]
-
+brainStr = ''.join([str(axon) for axon in basicBrain])
 # '002effff' + # flee! if other.deadliness is higher
 # '032fffff' + # mate if other is similar to self
 # '06300000' + # don't eat if you're not hungry
