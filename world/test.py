87b743825abe7b24b4875da975560d1b4fe87649
jepidoptera <jarymoth@gmail.com>
2022-02-22 01:23:45 -0600

    importable testbed world

--- world/test.py
+++ world/test.py
@@ -1,11 +1,16 @@
-from map import *
+from world.map import *
+from creatures.creature import *
+from creatures.genome import *
+from creatures import templates
+
 mapWidth = 30
 mapHeight = 20
 centerTile = int(mapWidth * mapHeight / 2 + mapHeight / 2)
 testmap = Map(mapWidth, mapHeight)
-testmap.printVisionCone(centerTile, 0, 0, 0)
-print()
 # calcVisionTree(testmap.nodes[146], 8)
 for n in range(6):
     testmap.printVisionCone(centerTile, n, 7, 2)
     print()
+print()
+testmap.printVisionCone(centerTile, 0, 0, 0)
+
