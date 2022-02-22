258da34f98df26c3a7ba71f60e4fc84e438bafad
jepidoptera <jarymoth@gmail.com>
2022-02-21 20:57:52 -0600

    split creature and genome tests

--- creature_test.py
+++ creature_test.py
@@ -4,45 +4,6 @@ from creatures.creature import *
 from creatures import templates
 from world.map import *
 
-class testGenome(unittest.TestCase):
-    gene = Genome(
-        energy=1, 
-        deadliness=1, 
-        speed=1, 
-        stamina=4, 
-        fortitude=4, 
-        intelligence=33, 
-        longevity=6, 
-        fertility=9, 
-        meateating=3, 
-        planteating=7, 
-        sightrange=5, 
-        sightfield=3,
-        mindStr='345979023qr79fa70450b0734ec3098e90283b'
-    )
-    def test(self):
-        print (self.gene.stamina)
-        self.assertTrue(self.gene.stamina == 3)
-        print ('raw')
-        print ()
-        print(*[f'{key}: {value.value}' for i, (key, value) in enumerate(self.gene.stats.items())], sep='\n')
-        print ()
-        print ('computed')
-        print ()
-        print(*[
-            f'energy: {self.gene.energy}', 
-            f'deadliness: {self.gene.deadliness}', 
-            f'speed: {self.gene.speed}', 
-            f'stamina: {self.gene.stamina}', 
-            f'fortitude: {self.gene.fortitude}', 
-            f'intelligence: {self.gene.intelligence}', 
-            f'longevity: {self.gene.longevity}', 
-            f'fertility: {self.gene.fertility}', 
-            f'sight range: {self.gene.sightRange}', 
-            f'field of view: {self.gene.sightField}', 
-            f'meat eating: {self.gene.meatEating}', 
-            f'plant eating: {self.gene.plantEating}'
-        ], sep='\n')
 
 class testCreature(unittest.TestCase):
     def test(self):
@@ -117,5 +78,4 @@ class testCreature(unittest.TestCase):
     # 9    29    49    69    89    109    129    149    169    189
     #   19    39    59    79    99    119    139    159    179    199
 
-testGenome().test()
 testCreature().test()
