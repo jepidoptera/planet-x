dbcd9af909b33ae0706314281a246502ac0d45db
jepidoptera <jarymoth@gmail.com>
2022-02-24 09:41:56 -0600

    generalized

--- world/life.py
+++ world/life.py
@@ -1,3 +1,4 @@
+from itertools import count
 from creatures import templates
 from world.map import *
 from creatures.creature import *
@@ -9,25 +10,144 @@ import json
 
 mapWidth: int=80
 mapHeight: int=40
-viewX: int=0
-viewY: int=0
 map = Map(mapWidth, mapHeight)
-# creatures = [Creature(random.choice(map.nodes), randomGenome(), randomGenome(), energy=32) for n in range(10)]
+loadfile = ''
+loadDir='species/'
 
-def main(stdscr):
+thoughtThreshold = 60
+moveThreshold = 60
+
+creatures: set[Creature]=set()
+
+def cycle(creatures: set[Creature]):
+
+    random.choice(map.nodes).resource = Resource(ResourceType.grass, 10)
+    aliveCreatures: set[Creature] = set()
+    for creature in creatures:
+        
+        creature.thinkTimer += creature.intelligence/2
+        if creature.thinkTimer > thoughtThreshold:
+            creature.thinkTimer -= thoughtThreshold
+            creature.think()
+
+        creature.moveTimer += creature.speed
+        if creature.moveTimer > moveThreshold:
+            creature.moveTimer -= moveThreshold
+            creature.animate()
+
+        creature.age += 1
+        creature.energy -= creature.metabolism
+        if creature.age > creature.longevity * 100 or creature.energy < 0:
+            creature.die()            
+
+        if not creature.dead: aliveCreatures.add(creature)
+        if creature.offspring: 
+            aliveCreatures.add(creature.offspring)
+            creature.offspring=None
+
+    return aliveCreatures
+
+def countSpecies(creatures):
+    species={}
+    for creature in creatures:
+        # count surviving species
+        if not creature.speciesName in species:
+            species[creature.speciesName]=0
+        species[creature.speciesName] += 1
+
+    topSpecies = [(name, number) for i, (name, number) in enumerate(species.items())]
+    topSpecies.sort(key=lambda x: -x[1])
+    return topSpecies
+
+def animate(stdscr):
+    if loadfile:
+        creatures = loadCreatures(loadDir + loadfile)
+    else:
+        creatures = generateCreatures()
+    
     # Clear screen
     stdscr.clear()
-    # stdscr.nodelay(True)
+    stdscr.nodelay(True)
+    stdscr.keypad(True)
     
     over=False
     steps=0
-    # the progenitors of all life
+
+    species=[]
+
+    viewX: int=0
+    viewY: int=0
+    viewWidth: int=80
+    viewHeight: int=40
+
+    while not over:
+
+        creatures = cycle(creatures)
+
+        steps+=1
+        if steps % 10 == 0:
+            species=countSpecies(creatures)
+
+        stdscr.addstr(0, 0, str(steps))
+        stdscr.refresh()
+        inKey=stdscr.getch()
+        if inKey==ord('q'): over=True
+        if inKey==ord('s'): save(creatures, f'species/{species[0][0]} {steps}.txt')
+        if inKey==curses.KEY_UP: viewY = max(viewY-1, 0)
+        if inKey==curses.KEY_DOWN: viewY = min(viewY+1, mapHeight-viewHeight)
+        if inKey==curses.KEY_LEFT: viewX = max(viewX-1, 0)
+        if inKey==curses.KEY_RIGHT: viewX = min(viewX+1, mapWidth-viewWidth)
+        
+        for y in range(viewY, viewY + min(curses.LINES, mapHeight-1)):
+            row = [
+                map.nodes[x*mapHeight*2 + (y % 2)*mapHeight + y//2] 
+                for x in range(viewX, viewX + min(curses.COLS//5 - 2, viewWidth//2-1))
+            ]
+            line = '' if y % 2 == 0 else '   '
+            for node in row:
+                if node.occupant:
+                    line += (
+                        node.occupant.speciesName[0].upper() if node.occupant.meateating > node.occupant.planteating 
+                        else node.occupant.speciesName[0]
+                    )
+                elif node.resource:
+                    line += '+' if node.resource.type == ResourceType.meat else '"'
+                else:
+                    line += '.'
+                line += '     '
+            stdscr.addstr(y, 0, line)
+
+        # show top existing species
+        stdscr.addstr(viewHeight, 0, f'total:{len(creatures)}')
+        for y in range(min(3, len(species))):
+            stdscr.addstr(viewHeight+1+y, 0, f'{species[y][0]}: {species[y][1]}')
+    
+    # print (steps)
+    # print (*[f'{species}: {number}' for i, (species, number) in enumerate(species.items())], sep='\n')
+
+def save(creatures: set[Creature], filename: str):
+    with open(filename, 'w') as file:
+        json.dump([creature.toJson() for creature in creatures], file, indent=4)
+
+def loadCreatures(filename: str) -> set[Creature]:
+    print(f'loading {filename}')
+    with open(filename, 'r') as file:
+        creaturedata = json.load(file)
+        creatures = set([
+            Creature.fromJson(c, map.nodes[c['location']])
+            for c in creaturedata
+        ])
+    return creatures
+
+def generateCreatures():
+    print(f'generating 300 random creatures...')
     herb = templates.herbivore()
     carn = templates.carnivore()
     # now add some random in the mix
+    # 3/4 prefab, 1/4 rando
     creatures: set[Creature] = (
-        [templates.cross(herb, templates.rando(), random.choice(map.nodes)) for n in range(100)] 
-        + [templates.cross(carn, templates.rando(), random.choice(map.nodes)) for n in range(20)]
+        [templates.cross(herb, templates.cross(herb, templates.rando()), random.choice(map.nodes)) for n in range(200)] 
+        + [templates.cross(carn, templates.cross(carn, templates.rando()), random.choice(map.nodes)) for n in range(100)]
     )
     # they have served their purpose
     del(herb)
@@ -36,69 +156,7 @@ def main(stdscr):
     for creature in creatures:
         creature.age = random.random() * 90 * creature.longevity
 
-    thoughtThreshold = 60
-    moveThreshold = 60
-    
-    while not over:
-        random.choice(map.nodes).resource = Resource(ResourceType.grass, 10)
-        aliveCreatures: set[Creature] = set()
-        species={}
-        topspecies=[]
-        steps+=1
-        for creature in creatures:
-            # count surviving species
-            if not creature.speciesName in species:
-                species[creature.speciesName]=0
-            species[creature.speciesName] += 1
-            
-            # worldPad.addstr(int(creature.location.y), int(creature.location.x*4), '.')
-            # if (creature.location.x*4 < curses.COLS and creature.location.y*2 + creature.location.x%2 < curses.LINES):
-            #     stdscr.addstr(round(creature.location.y*2 + creature.location.x%2), round(creature.location.x * 4), '.')
-            creature.thinkTimer += creature.intelligence
-            if creature.thinkTimer > thoughtThreshold:
-                creature.thinkTimer -= thoughtThreshold
-                creature.think()
+    return creatures
 
-            creature.moveTimer += creature.speed
-            if creature.moveTimer > moveThreshold:
-                creature.moveTimer -= moveThreshold
-                creature.animate()
-
-            if not creature.dead: aliveCreatures.add(creature)
-            if creature.offspring: 
-                aliveCreatures.add(creature.offspring)
-                creature.offspring=None
-            # worldPad.addstr(int(creature.location.y), int(creature.location.x*4), creature.speciesName[0])
-            # if (creature.location.x*4 < curses.COLS and creature.location.y*2 + creature.location.x%2 < curses.LINES):
-            #     stdscr.addstr(round(creature.location.y*2 + creature.location.x%2), round(creature.location.x * 4), creature.speciesName[0])
-        creatures = aliveCreatures
-        # print (f'creature 0 at: {creatures[0].location.x, creatures[0].location.y}')
-
-        stdscr.addstr(0, 0, str(steps))
-        stdscr.refresh()
-        inKey=stdscr.getch()
-        if inKey==ord('q'): over=True
-        for y in range(viewY, min(curses.LINES, mapHeight-1)):
-            row = [map.nodes[x*mapHeight*2 + (y % 2)*mapHeight + y//2] 
-                for x in range(viewX, min(curses.COLS//5 - 2, mapWidth//2-1))]
-            line = '' if y % 2 == 0 else '   '
-            for node in row:
-                if node.occupant:
-                    line += node.occupant.speciesName[0].upper() if node.occupant.victim else node.occupant.speciesName[0]
-                elif node.resource:
-                    line += '+' if node.resource.type == ResourceType.meat else '"'
-                else:
-                    line += '.'
-                line += '     '
-            stdscr.addstr(y, 0, line)
-        topSpecies = [(name, number) for i, (name, number) in enumerate(species.items())].sort(key=lambda x: x[1])
-        for y in range(3):
-            stdscr.addstr(40+y, 0, f'{topSpecies[0]}: {topSpecies[1]}')
-    
-    print (steps)
-    print (*[f'{species}: {number}' for i, (species, number) in enumerate(species.items())], sep='\n')
-wrapper(main)
-
-def save(creatures: set[Creature], filename: str):
-    with open(filename, 'w') as file:
-        file.write(json.dumps(creatures))
+def run():
+    wrapper(animate)
