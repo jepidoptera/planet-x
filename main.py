aa9dca7a3981bce2a00e89f53ec9175db9993320
jepidoptera <jarymoth@gmail.com>
2022-02-24 19:53:08 -0600

    command line interface

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
+from fileinput import filename
+import sys, getopt
 from world import life
+from world import stilllife
+still = True
+filename = ''
+
+argList = sys.argv[1:]
+args, values = getopt.getopt(argList, 'af:')
+for arg, value in args:
+    if arg == '-f':
+        filename=value
+    elif arg == '-a':
+        still = False
+
+if still:
+    stilllife.run(loadfile=filename)
+else:
+    life.loadfile=filename
+    life.run()
